f15e7644354df57a39c21820003aab10bd75dc4f
FrankLueza <ixfanalytics@gmail.com>
2018-05-31 16:42:08 -0500

    agregando clientes y modificando nav

--- js/init.js
+++ js/init.js
@@ -7,10 +7,14 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
+/*
 $('.carousel.carousel-slider').carousel({
   fullWidth: true,
   indicators: true
-});
+});*/
 
 $(document).ready(function(){
   $('.modal').modal();
