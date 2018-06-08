5717b7a5d1c43d72937985250f3d641a6fc7b22f
FrankLueza <ixfanalytics@gmail.com>
2018-06-07 21:24:22 -0500

    cambiando el sitio con Materialize

--- js/init.js
+++ js/init.js
@@ -1,24 +1,41 @@
-(function($){
-  $(function(){
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-$(document).ready(function(){
-  $('.carousel').carousel();
+$(function(){
+  $('.scrollspy').scrollSpy();
 });
-/*
+
 $('.carousel.carousel-slider').carousel({
   fullWidth: true,
   indicators: true
-});*/
+});
+$(document).ready(function(){
+  $('.slider').slider({
+    indicators: false,
+    duration: 500,
+    interval: 3000,
+    height: 700,
+  });
+});
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
 
 $(document).ready(function(){
   $('.modal').modal();
 });
 $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
+  $('.tap-target').tapTarget();
+});
+$(document).ready(function(){
+  $('.materialboxed').materialbox();
+});
+$(document).ready(function(){
+  $('.carousel').carousel({
+    duration: 200,
+    dist: -200,
+    shift: 100
+    
   });
+});
