008dc009a194e641df890a0f5fb00a6c724399ff
FrankLueza <ixfanalytics@gmail.com>
2018-06-09 02:23:09 -0500

    colocando boton fijo de scroll a inicio de pag

--- js/init.js
+++ js/init.js
@@ -40,3 +40,19 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+$(document).ready(function(){
+ 
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+$(window).scroll(function(){
+    if( $(this).scrollTop() > 0 ){
+        $('.ir-arriba').slideDown(300);
+    } else {
+        $('.ir-arriba').slideUp(300);
+    }
+    });
+});
