1d79a8863b452f5ea62f56d877451aa17ca5aaef
FrankLueza <ixfanalytics@gmail.com>
2018-06-09 02:45:05 -0500

    colviendo a colocar el boton de scroll de inicio

--- js/init.js
+++ js/init.js
@@ -56,3 +56,16 @@ $(window).scroll(function(){
     }
     });
 });
+$('.ir-arriba').click(function(){
+  $('body, html').animate({
+      scrollTop: '0px'
+  }, 1000);
+});
+
+$(window).scroll(function(){
+  if( $(this).scrollTop() > 0 ){
+      $('.ir-arriba').slideDown(600);
+  } else {
+      $('.ir-arriba').slideUp(600);
+  }
+});
