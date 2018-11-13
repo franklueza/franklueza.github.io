3b626252980fa780e346bbfe588936e937ed5e8f
franklueza <ixfanalytics@gmail.com>
2018-11-13 00:39:41 -0600

    agregando menu y animaciones6

---
+++ js/animatereveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.headline')
+ScrollReveal().reveal('.tagline', { delay: 500 })
+ScrollReveal().reveal('.punchline', { delay: 1000 })
+ScrollReveal().reveal('.typing', { delay: 1250 })
+ScrollReveal().reveal('.widget', { interval: 500 });
