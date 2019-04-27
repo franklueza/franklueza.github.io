68d88cfc35b99e6f7ea75bfecbe22f309cf6b36e
franklueza <ixfanalytics@gmail.com>
2019-04-27 14:25:11 -0500

    Nuevo sitio

--- js/animatereveal.js
+++
@@ -1,5 +0,0 @@
-ScrollReveal().reveal('.headline')
-ScrollReveal().reveal('.tagline', { delay: 500 })
-ScrollReveal().reveal('.punchline', { delay: 1000 })
-ScrollReveal().reveal('.typing', { delay: 1250 })
-ScrollReveal().reveal('.widget', { interval: 500 });
--- js/scroll.js
+++
@@ -1,5 +0,0 @@
-
-//https://github.com/cferdinandi/smooth-scroll
-var easeInQuart = new SmoothScroll('[data-easing="easeInQuart"]', {easing: 'easeInQuart'});
-
-
